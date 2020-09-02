07718a470cb949c6a156907f6b75aeab79106d8b
kyu929 <j2mity@gmail.com>
2020-09-02 23:28:21 +0900

    Modified models

--- Riot_Website/polls/models.py
+++ Riot_Website/polls/models.py
@@ -1,13 +1,22 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.title
     
 # Create your models here.
